26a91c3b5dbd23683f5f09ad424fcf9161eee5c9
Alexander Adler <alexander.adler0@googlemail.com>
2013-02-04 16:45:18 +0100

    remove stop words

--- features.py
+++ features.py
@@ -2,8 +2,10 @@
 
 from nltk import word_tokenize
 from collections import deque
+import email.parser
 
 SPECIAL_CHARS = "@`!\"#$%&´()*:+;[{,<\|-=]}.>^~/?_"
+MAIL_PARSER = email.parser.Parser()
 
 # tokenized
 def fractionCapitals(tokens):
@@ -14,13 +16,13 @@ def fractionCapitals(tokens):
     total = len(tokens)
     rv = float(capitals)/float(total)
     rv = int(1000*rv)
-    return {"capital-only tokens (promille)": rv}
+    return {"capital-only tokens (promille) ": rv}
 
 # not tokenized
 def fractionSpecialChars(text):
     rv = {}
     total = len(text)
-    PREFIX = "special char (promille)"
+    PREFIX = "special char (promille) "
     for char in SPECIAL_CHARS:
         rv[PREFIX + char] = 0
 
@@ -57,7 +59,7 @@ def fractionDigits(tokens):
 def trigrams(text):
 	rd = {}
 	aux = deque(maxlen=3)
-	for char in text:
+	for char in text.lower():
 		aux.append(char)
 		if len(aux) > 2:
 			trigram = ''.join(aux)
@@ -66,3 +68,48 @@ def trigrams(text):
 			else:
 				rd['trigram - '+trigram] = 1
 	return rd
+
+# adds (overwriting) all keys from new to old
+def addToDict(old, new):
+    for (key, value) in new.items():
+        old[key] = value
+
+TOKEN_FUNCTIONS = [fractionCapitals, fractionDigits]
+TEXT_FUNCTIONS  = [fractionSpecialChars, trigrams]
+HTML_FUNCTIONS  = []
+STOP_WORDS      = set() # read it later
+
+with open("english_stop_words.txt") as f:
+    for word in f:
+        word = word.strip()
+        STOP_WORDS.add(word)
+
+STOP_WORDS = frozenset(STOP_WORDS)
+
+def featuresForMail(path):
+    p = MAIL_PARSER
+    rv = {}
+    with open(path) as f:
+        mail = p.parse(f)
+
+# text of the email
+        fullText = ""
+        for part in mail.walk():
+            if not part.is_multipart(): # TODO: handle HTML
+                fullText += "\n" + part.get_payload(decode=True)
+
+        tokens = word_tokenize(fullText)
+
+# remove stop words
+        tokens = filter(lambda token: token not in STOP_WORDS, tokens)
+        fullText = " ".join(tokens)
+
+        for function in TEXT_FUNCTIONS:
+            data = function(fullText)
+            addToDict(rv, data)
+
+        for function in TOKEN_FUNCTIONS:
+            data = function(tokens)
+            addToDict(rv, data)
+
+        return rv
