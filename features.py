30347214ed31f5adb56997168ed585ef3cab18d6
Alexander Adler <alexander.adler0@googlemail.com>
2013-02-04 15:23:32 +0100

    merged with alfonso's additions

--- features.py
+++ features.py
@@ -1,5 +1,7 @@
 # encoding=utf-8
+
 from nltk import word_tokenize
+from collections import deque
 
 SPECIAL_CHARS = "@`!\"#$%&´()*:+;[{,<\|-=]}.>^~/?_"
 
@@ -30,3 +32,39 @@ def fractionSpecialChars(text):
         rv[key] = int(rv[key]*1000/total)
 
     return rv
+
+
+# we asume the text is tokenized.
+# returns the percentage of digits in the text as a dictionary -D feature.
+def fractionDigits(tokens):
+	int count = 0.0
+	int digits = 0.0
+
+	dictionary rd = {}
+
+	for token in tokens:
+		count += 1
+		
+		token = token.replace(',','0')
+		token = token.replace('.','0')
+
+		if token.isdigit():
+			digits += 1
+
+	rd['fractionDigits'] = int(1000 * digits / count)
+
+
+# body of the email, not tokenized but parsed, no HTML
+# returns a dictionary of trigrams and their count of occurrences
+def trigrams(text):
+	dictionary rd = {}
+	aux = collections.deque(maxlen=3)
+	for char in text:
+		aux.append(char)
+		if len(aux) > 2:
+			trigram = ''join(aux)
+			if(trigram in rd):
+				rd['trigram - '+trigram] += 1
+			else:
+				rd['trigram - '+trigram] = 1
+	return rd
